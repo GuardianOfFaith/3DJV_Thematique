a72ffd8b82c19dcc28738ece0238555b902524df
Angèle <nicolas.besso@aliceadsl.fr>
2018-10-15 17:52:00 +0200

    Changement de niveau
    permet de side scroll quand un joueur atteint le bord de l'écran.
    Trigger fonctionnel.
    TODO SideScroll, repositionnement des personnages, et vérifications (win/joueurOff)

---
+++ Assets/Transition.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Transition : MonoBehaviour {
+
+    void Start()
+    {
+
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        Debug.Log("GameObject2 collided with " + col.name);
+    }
+
+}
