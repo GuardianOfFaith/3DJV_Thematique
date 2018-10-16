dfec1cae374c02adc413441f3f1d142fdc5045de
GuardianOfFaith <nicolas.besso@aliceadsl.fr>
2018-10-16 16:10:21 +0200

    Random routine
    ajout de la routine random pour l'IA

--- Assets/Script/player2Controller.cs
+++ Assets/Script/player2Controller.cs
@@ -12,15 +12,15 @@ public class player2Controller : MonoBehaviour
 	[SerializeField] private fire2Script fire;
 
 	private Vector2 move = Vector2.zero;
-	
+    public int rand;
 	private bool facingDirection=true;
 	
 	void Update ()
 	{	
-		move.x = Input.GetAxis("HorizontalX1");
-		move.y = Input.GetAxis("VerticalY1");
-	
-	
+		//move.x = Input.GetAxis("HorizontalX1");
+		//move.y = Input.GetAxis("VerticalY1");
+
+        
 		if (move.x>0 && !facingDirection)
 		{
 			flipDirection();
@@ -39,7 +39,8 @@ public class player2Controller : MonoBehaviour
 
 	void FixedUpdate()
 	{
-		rbSpatialShip.velocity = move * speed;
+        random();
+        rbSpatialShip.velocity = move * speed;
 	}
 	
 	// tourner le vaisseau vers la direction appuyée
@@ -48,4 +49,29 @@ public class player2Controller : MonoBehaviour
 		spatialShip.Rotate(0f,180f,0f);
 		facingDirection = !facingDirection;
 	}
+
+
+    void random()
+    {
+        rand = Mathf.RoundToInt(Random.value * 5);
+        
+        switch (rand)
+        {
+            case 1:
+                move.x = -1;
+                break;
+            case 2:
+                move.y = 1;
+                break;
+            case 3:
+                move.y = -1;
+                break;
+            case 4:
+                StartCoroutine(fire.shoot());
+                break;
+            default:
+                move.x = 1;
+                break;
+        }
+    }
 }
