78ee842573900accbf1982e946f848543aec7c26
GuardianOfFaith <nicolas.besso@aliceadsl.fr>
2018-10-16 10:54:27 +0200

    Transition
    Permet de changer d'écran,
    Possède les conditions de victoire
    
    TODO ajouté check Domination, respawn, et fin de partie

--- Assets/Transition.cs
+++ Assets/Transition.cs
@@ -4,14 +4,59 @@ using UnityEngine;
 
 public class Transition : MonoBehaviour {
 
+    public Camera cam;
+    public int speed = 4;
+    public int arena;
+    public int i;
+    public GameObject p1;
+    public GameObject p2;
+    public GameObject S1;
+    public GameObject S2;
+
     void Start()
     {
-
+        arena = 0;
+        cam = Camera.main;
+        p1 = GameObject.FindWithTag("Player1");
+        p2 = GameObject.FindWithTag("Player2");
+        i = 11;
     }
 
     void OnTriggerEnter2D(Collider2D col)
     {
-        Debug.Log("GameObject2 collided with " + col.name);
+        if (arena == 0 && this.tag == ("TriggerOff") && col.CompareTag("Player1")/* && TODO DOMINATE*/)
+        {
+            cam.transform.position = new Vector3(cam.transform.position.x + i, cam.transform.position.y, cam.transform.position.z);
+            //SetPosition to P2
+            arena++;
+        }
+        if (arena == 1 && this.tag == ("TriggerOff") && col.CompareTag("Player2")/* && TODO DOMINATE*/)
+        {
+            cam.transform.position = new Vector3(cam.transform.position.x - i, cam.transform.position.y, cam.transform.position.z);
+            //SetPosition to P1
+            arena--;
+        }
+        if (arena == 0 && this.tag == ("TriggerDef") && col.CompareTag("Player2")/* && TODO DOMINATE*/)
+        {
+            cam.transform.position = new Vector3(cam.transform.position.x - i, cam.transform.position.y, cam.transform.position.z);
+            //SetPosition to P1
+            arena--;
+        }
+        if (arena == -1 && this.tag == ("TriggerDef") && col.CompareTag("Player1")/* && TODO DOMINATE*/)
+        {
+            cam.transform.position = new Vector3(cam.transform.position.x - i, cam.transform.position.y, cam.transform.position.z);
+            //SetPosition to P1
+            arena++;
+        }
+
+        if (arena == -1 && this.tag == ("TriggerDef") && col.CompareTag("Player2")/* && TODO DOMINATE*/)
+        {
+            //TODO CALL LOSE
+        }
+        if (arena == 1 && this.tag == ("TriggerOff") && col.CompareTag("Player1")/* && TODO DOMINATE*/)
+        {
+            //TODO CALL WIN;
+        }
     }
 
 }
