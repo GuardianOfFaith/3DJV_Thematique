bf016f7a2f4c554ea96c804893d3fb973786762c
AranHaion <44162103+AranHaion@users.noreply.github.com>
2018-10-17 13:57:56 +0200

    Add files via upload

--- Assets/Script/player2Controller.cs
+++ Assets/Script/player2Controller.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using System.Runtime.Serialization.Formatters;
 using UnityEditor;
 using UnityEngine;
@@ -42,8 +43,9 @@ public class player2Controller : MonoBehaviour
 
 	void FixedUpdate()
 	{
-        random();
+        RRAgent();
         rbSpatialShip.velocity = move * speed;
+
 	}
 	
 	// tourner le vaisseau vers la direction appuyée
@@ -53,21 +55,22 @@ public class player2Controller : MonoBehaviour
 		facingDirection = !facingDirection;
 	}
 
-
     void random()
     {
+
         rand = Mathf.RoundToInt(Random.value * 5);
-        
+
         switch (rand)
         {
             case 1:
                 move.x = -1;
+                
                 break;
             case 2:
                 move.y = 1;
                 break;
             case 3:
-                move.y = -1;
+                move.y = - 1;
                 break;
             case 4:
                 StartCoroutine(fire.shoot());
@@ -78,20 +81,105 @@ public class player2Controller : MonoBehaviour
         }
     }
 
+    int random(Vector3 pos)
+    {
+        
+        rand = Mathf.RoundToInt(Random.value * 5);
+        
+        switch (rand)
+        {
+            case 1:
+                //pos = new Vector3((pos.x - 1), pos.y, pos.z);
+                pos.x = pos.x - 1;
+                break;
+            case 2:
+                pos.y = pos.y + 1;
+                break;
+            case 3:
+                pos.y = pos.y - 1;
+                break;
+            case 4:
+                StartCoroutine(fire.shoot());
+                break;
+            default:
+                pos.x = pos.x + 1;
+                break;
+        }
+        Debug.Log(rand);
+        return rand;
+    }
+
     void RRAgent()
     {
-        //observe
-        int y = 0;
-        GameObject advClone = Instantiate(GameObject.FindWithTag("Player1"));
-        GameObject usClone = Instantiate(GameObject.FindWithTag("Player2"));
+        int[] actionList = new int[5] { 0,0,0,0,0 };
 
-        while (y < i){
+        for (int z = 0; z < 5; z++)
+        { 
+            int y = 0;
+            Vector3 advCPos = GameObject.FindWithTag("Player1").transform.position;
+            int advRew = 0;
+            Vector3 usCPos = GameObject.FindWithTag("Player2").transform.position;
+            int usRew = 0;
 
-            advClone.GetComponent<player2Controller>().random();
-            usClone.GetComponent<player2Controller>().random();
+            //bool dominate;
+            
+            while (y < i){
+                Reward(random(advCPos), true, advRew);
+                Reward(random(usCPos), false, usRew);
+                Debug.Log(usCPos);
+                Debug.Log(usRew);
+            }
+            actionList[z] = usRew;
+            
+        }
+        
+        switch (actionList[System.Array.IndexOf(actionList, actionList.Max())])
+        {
+            case 0:
+                move.x = - 1;
+                break;
+            case 1:
+                move.y = 1;
+                break;
+            case 2:
+                move.y = - 1;
+                break;
+            case 3:
+                StartCoroutine(fire.shoot());
+                break;
+            default:
+                move.x = 1;
+                break;
         }
 
-        Destroy(adv);
-        Destroy(us);
+
+    }
+
+    void Reward(int state, bool atk, int rew)
+    {
+        switch (state)
+        {
+            case 1:
+                if (!atk)
+                {
+                    rew = rew + 3;
+                }
+                break;
+            case 2:
+                rew++;
+                break;
+            case 3:
+                rew++;
+                break;
+            case 4:
+                //Check touch
+                break;
+            default:
+                if (atk)
+                {
+                    rew = rew + 3;
+                }
+                break;
+        }
     }
 }
